5dd19d501031744621129064e6c04a24441cd30c
Irfan Nada Bayu Samudera <irfannadabayusamudera@gmail.com>
2022-07-06 09:17:02 +0700

    add jwt from oauth, bug fix and improvement

--- index.js
+++ index.js
@@ -10,26 +10,17 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const initializePassport = require("./config/passport");
 
-initializePassport(passport);
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(
-  cookieSession({
-    name: "session-app",
-    secret: "uiafhsadifjasdifjnpjasdfhuaslfhdi",
-
-    // Cookie Options
-    maxAge: 24 * 60 * 60 * 1000, // 24 hours
-  })
-);
 app.use(passport.initialize());
-app.use(passport.session());
 app.use("/api/v1", router);
 app.use("/upload/images", express.static("./upload/images"));
 
 // listen on port
-app.listen(PORT, () => console.log("Server running at http://localhost:", PORT));
+app.listen(PORT, () =>
+  console.log("Server running at http://localhost:", PORT)
+);
--- routes/index.js
+++ routes/index.js
@@ -22,12 +22,12 @@ const {
 } = require("../controllers/TransactionController");
 const { authorize } = require("../middleware/Authorize");
 const { verifyToken } = require("../middleware/verifyToken");
-const { newToken } = require("../controllers/NewToken");
+const { signToken } = require("../services/authService");
+const { newToken } = require("../services/authService");
 const multer = require("multer");
 const path = require("path");
 const router = express.Router();
 const passport = require("passport");
-
 // handle storage using multer
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -41,7 +41,7 @@ let storage = multer.diskStorage({
   },
 });
 
-let upload = multer({ 
+let upload = multer({
   storage: storage,
   limits: { fileSize: "10000000" },
   fileFilter: (req, file, cb) => {
@@ -53,7 +53,7 @@ let upload = multer({
       return cb(null, true);
     }
     cb("File format not allowed!");
-  }, 
+  },
 });
 
 // Auth Router
@@ -62,39 +62,36 @@ router.get("/user/:id", verifyToken, getUserById);
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
+
+//GET LOGOUT ONLY FOR LOGOUT WITHOUT FORM
 router.get("/logout", (req, res) => {
   req.logout();
   res.status(200).send("Logout Success");
 });
+
 router.get("/whoami", verifyToken, whoami);
-router.put("/profile/update", [verifyToken, upload.single("image")], updateProfile);
+router.put(
+  "/profile/update",
+  [verifyToken, upload.single("image")],
+  updateProfile
+);
 router.get("/token", newToken);
 
 //Auth Google
 router.get(
   "/auth/google",
   passport.authenticate("google", {
+    session: false,
     scope: ["profile", "email"],
   })
 );
 router.get(
   "/auth/google/callback",
-  passport.authenticate("google", {
-    successRedirect: "/api/v1/login/success",
-    failureRedirect: "/api/v1/login/failed/",
-  }),
+  passport.authenticate("google", { session: false }),
   (req, res) => {
-    res.status(200).json({ success: "User logged in" });
+    signToken(req, res);
   }
 );
-
-router.get("/login/success", (req, res) => {
-  res.status(200).send("Login Berhasil!. Logout: GET /api/v1/logout");
-});
-router.get("/login/failed", (req, res) => {
-  res.send(400).json("Something went wrong");
-});
-
 // Product Router
 router.get("/products", handler.getAllProduct);
 router.get("/products/:id", handler.getProduct);
